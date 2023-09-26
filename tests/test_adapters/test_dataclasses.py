7b2de994e66d658d77d96c17e3bb0f7c5ae2be53
Piotr Kilczuk <piotr@kilczuk.dev>
2023-09-26 08:49:16 +0200

    More robust tests for dataclasses

--- tests/test_adapters/test_dataclasses.py
+++ tests/test_adapters/test_dataclasses.py
@@ -14,6 +14,8 @@ my_registry = MyRegistry("my_registry")
 
 
 def test_works_with_frozen_eq_dataclass():
+    my_registry.clear()
+
     @dataclasses.dataclass(frozen=True, eq=True)
     class MyDataclass(pyreg_dataclasses.RegisteredDataclass):
         str_field: str
@@ -25,9 +27,14 @@ def test_works_with_frozen_eq_dataclass():
     my_dataclass = MyDataclass(str_field="a", int_field=1)
 
     assert my_dataclass in my_registry
+    assert my_registry.all() == {my_dataclass}
+    assert my_registry.filter(str_field="a") == {my_dataclass}
+    assert my_registry.filter(int_field=0) == set()
 
 
 def test_works_with_frozen_but_not_eq_dataclass():
+    my_registry.clear()
+
     @dataclasses.dataclass(frozen=True, eq=False)
     class MyDataclass(pyreg_dataclasses.RegisteredDataclass):
         str_field: str
@@ -39,9 +46,14 @@ def test_works_with_frozen_but_not_eq_dataclass():
     my_dataclass = MyDataclass(str_field="a", int_field=1)
 
     assert my_dataclass in my_registry
+    assert my_registry.all() == {my_dataclass}
+    assert my_registry.filter(str_field="a") == {my_dataclass}
+    assert my_registry.filter(int_field=0) == set()
 
 
 def test_works_with_unsafe_hash():
+    my_registry.clear()
+
     @dataclasses.dataclass(unsafe_hash=True)
     class MyDataclass(pyreg_dataclasses.RegisteredDataclass):
         str_field: str
@@ -53,9 +65,14 @@ def test_works_with_unsafe_hash():
     my_dataclass = MyDataclass(str_field="a", int_field=1)
 
     assert my_dataclass in my_registry
+    assert my_registry.all() == {my_dataclass}
+    assert my_registry.filter(str_field="a") == {my_dataclass}
+    assert my_registry.filter(int_field=0) == set()
 
 
 def test_works_with_custom_hash():
+    my_registry.clear()
+
     @dataclasses.dataclass
     class MyDataclass(pyreg_dataclasses.RegisteredDataclass):
         str_field: str
@@ -70,9 +87,14 @@ def test_works_with_custom_hash():
     my_dataclass = MyDataclass(str_field="a", int_field=1)
 
     assert my_dataclass in my_registry
+    assert my_registry.all() == {my_dataclass}
+    assert my_registry.filter(str_field="a") == {my_dataclass}
+    assert my_registry.filter(int_field=0) == set()
 
 
 def test_does_not_work_with_non_hashable():
+    my_registry.clear()
+
     @dataclasses.dataclass
     class MyDataclass(pyreg_dataclasses.RegisteredDataclass):
         str_field: str
