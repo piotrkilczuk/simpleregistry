ea64e057416a2df97fa25ef218439f76f59225cc
Piotr Kilczuk <piotr@kilczuk.dev>
2023-09-27 12:52:12 +0200

    Test that all README Python code is working

---
+++ tests/test_readme.py
@@ -0,0 +1,27 @@
+import hashlib
+import pathlib
+import re
+from typing import List
+
+import pytest
+
+README_PATH = pathlib.Path(__file__).parent.parent / "README.md"
+README_CHECKSUM_ALLOWED = "87fa73dd25838ea8931b6a02b110f590"
+
+
+@pytest.fixture
+def python_snippets():
+    readme_contents_bytes = README_PATH.read_bytes()
+
+    hash_md5 = hashlib.md5(readme_contents_bytes)
+    assert hash_md5.hexdigest() == README_CHECKSUM_ALLOWED
+
+    code_snippets = re.findall(
+        r"```python(.*?)```", readme_contents_bytes.decode(), re.DOTALL
+    )
+    yield code_snippets
+
+
+def test_all_snippets_working(python_snippets: List[str]):
+    for snippet in python_snippets:
+        exec(snippet)
